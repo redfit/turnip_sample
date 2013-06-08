bcb7b8ea16296012e34ba0b27a0710972ef20228
fuchiwaki <fuchiwaki@kndb.jp>
2013-06-08 13:34:45 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurnipSample::Application.config.secret_token = '2280b5e609e877147d1a26c7d62aaaf8f3346d1411fef1c3e8ed58322143850f1d64322b59aedcfcb5f6cb210a8506f45437c4e3951dfc9fa096decf2c63dbaf'
